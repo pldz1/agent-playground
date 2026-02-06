bec32bab728b91a1930674f8f15dfd189e2485ba
pldz1 <pldzmsoone@outlook.com>
2026-02-06 15:01:16 +0800

    🔨 Refactor the file structure.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client';
 import App from './app/App';
-import './styles/index.css';
+import './assets/styles/index.css';
 import 'md-editor-rt/lib/style.css';
 import { initializeStore } from './store';
 
